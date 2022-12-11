266398829f62a3d154b9194487377d2e7bed9572
norbinorbert <47588133+norbinorbert@users.noreply.github.com>
2022-12-11 17:27:27 +0200

    Add files via upload

---
+++ Algoritmika/Hazi_csomag_4/TizenhatosRek/TizenhatosRek.cpp
@@ -0,0 +1,61 @@
+﻿/*Boda Norbert
+511-es csoport
+Lab4/08
+Írjunk rekurzív algoritmust, amely átalakít egy, a 16-os számrendszerben
+megadott egész számot a 10-es számrendszerbe! Ne használjunk ismétlő struktúrát a beolvasáson kívül!*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void beolvas(string& hexa) {
+	cin >> hexa;
+}
+
+bool helytelen(string& hexa, int index) {
+	hexa[index] = toupper(hexa[index]);
+	if (hexa[index] < '0' || (hexa[index] > '9' && hexa[index] < 'A') || hexa[index] >'F') {
+		return true;
+	}
+	if (index < hexa.size()-1) {
+		return helytelen(hexa, index + 1);
+	}
+	return false;
+}
+
+unsigned long long atalakit(string& hexa, int index) {
+	hexa[index] = toupper(hexa[index]);
+	if (hexa[index] >= '0' && hexa[index] <= '9') {
+		if (index == 0) {
+			return (hexa[index] - 48);
+		}
+		return atalakit(hexa, index - 1) * 16 + (hexa[index] - 48);
+	}
+	else {
+		if (index == 0) {
+			return (hexa[index] - 55);
+		}
+		return atalakit(hexa, index - 1) * 16 + (hexa[index] - 55);
+	}
+}
+
+int main() {
+	string hexa, temp;
+	beolvas(hexa);
+	short max_hossz = 16;
+	if (hexa.size() <= max_hossz) {
+		if (helytelen(hexa, 0)) {
+			cout << -1;
+		}
+		else {
+			unsigned long long megoldas = atalakit(hexa, hexa.size() - 1);
+			cout << megoldas;
+		}
+	}
+	else {
+		cout << -1;
+	}
+
+	return 0;
+}
---
+++ Algoritmika/Hazi_csomag_4/TorzstenyezokRek/TorzstenyezokRek.cpp
@@ -0,0 +1,69 @@
+﻿/*Boda Norbert
+511-es csoport
+Lab4/06
+Írjunk rekurzív algoritmust, amely kiír egy 𝑛 természetes számot törzstényezőkre
+bontva! Ne használjunk egyáltalán ismétlő struktúrát!
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void beolvas(unsigned int& szam) {
+	cin >> szam;
+}
+
+void torzstenyezok(unsigned int szam, unsigned int oszto, short hatvany) {
+	if (szam == 0) {
+		return;
+	}
+
+	if (szam == 1) {
+		if (hatvany > 1) {
+			cout << oszto << "^" << hatvany;
+		}
+		if (hatvany == 1) {
+			cout << oszto;
+		}
+		return;
+	}
+
+	if (hatvany == 0 && oszto > sqrt(szam)) {
+		cout << szam;
+		return;
+	}
+
+	if (szam % oszto == 0) {
+		return torzstenyezok(szam / oszto, oszto, hatvany + 1);
+	}
+	else {
+		if (hatvany > 1) {
+			cout << oszto << "^" << hatvany << "*";
+			hatvany = 0;
+		}
+		else if (hatvany == 1) {
+			cout << oszto << "*";
+			hatvany = 0;
+		}
+
+		if (oszto == 2) {
+			return torzstenyezok(szam, oszto + 1, hatvany);
+		}
+		//a prim osztok (6k-1) vagy (6k+1) alakuak lehetnek, ezert csak ezeket vizsgalja
+		if (oszto == 3 || (oszto + 1) % 6 == 0) {
+			return torzstenyezok(szam, oszto + 2, hatvany);
+		}
+		else {
+			return torzstenyezok(szam, oszto + 4, hatvany);
+		}
+	}
+}
+
+int main() {
+	unsigned int szam;
+	beolvas(szam);
+
+	torzstenyezok(szam, 2, 0);
+	return 0;
+}
