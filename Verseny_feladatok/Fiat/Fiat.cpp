599f7e2e6c0fc71e0ff3654a39252874cc7e7c30
norbinorbert <47588133+norbinorbert@users.noreply.github.com>
2023-01-14 22:41:04 +0200

    Add files via upload

---
+++ Verseny_feladatok/Fiat/Fiat.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+
+int main() {
+	int n;
+	cin >> n;
+	vector<unsigned long long> fakt(2 * n + 1);
+	fakt[0] = 1;
+	for (unsigned long long i = 1; i <= 2 * n; i++) {
+		fakt[i] = (fakt[i - 1] * i) % MOD;
+	}
+	unsigned long long szamlalo = fakt[2 * n];
+	unsigned long long nevezo = (fakt[n] * fakt[n + 1]) % MOD;
+	for (unsigned long long i = 1; i < MOD; i++) {
+		if ((nevezo * i) % MOD == 1) {
+			nevezo = i;
+			break;
+		}
+	}
+	cout << (szamlalo * nevezo) % MOD;
+	return 0;
+}
